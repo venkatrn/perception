9049bd5ec73e73132884c44f4404a59eff4d4aba
Venkatraman Narayanan <venkatrn@andrew.cmu.edu>
2016-07-06 18:23:04 -0400

    add kinect_sim camera_constants file

--- kinect_sim/include/kinect_sim/camera_constants.h
+++ kinect_sim/include/kinect_sim/camera_constants.h
@@ -1,22 +1,22 @@
 #pragma once
 
 // Parameters for MS Kinect V1.0
-// const int kCameraWidth = 640;
-// const int kCameraHeight = 480;
-// const float kCameraFX = 576.09757860f;
-// const float kCameraFY = 576.09757860f;
-// const float kCameraCX = 321.06398107f;
-// const float kCameraCY = 242.97676897f;
-// const float kZNear = 0.1f;
-// const float kZFar = 20.0f;
+const int kCameraWidth = 640;
+const int kCameraHeight = 480;
+const float kCameraFX = 576.09757860f;
+const float kCameraFY = 576.09757860f;
+const float kCameraCX = 321.06398107f;
+const float kCameraCY = 242.97676897f;
+const float kZNear = 0.1f;
+const float kZFar = 20.0f;
 
 // Parameters for MS Kinect V2.0 
 // APC: these values are from calib_ir.yaml
-const int kCameraWidth = 512;
-const int kCameraHeight = 424;
-const float kCameraFX = 354.76029132272464f;
-const float kCameraFY = 354.49153515383603f;
-const float kCameraCX = 253.06754087288363f;
-const float kCameraCY = 205.37746288838397f;
-const float kZNear = 0.1f;
-const float kZFar = 20.0f;
+// const int kCameraWidth = 512;
+// const int kCameraHeight = 424;
+// const float kCameraFX = 354.76029132272464f;
+// const float kCameraFY = 354.49153515383603f;
+// const float kCameraCX = 253.06754087288363f;
+// const float kCameraCY = 205.37746288838397f;
+// const float kZNear = 0.1f;
+// const float kZFar = 20.0f;
