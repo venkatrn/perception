70f98bbb184158c8f5b19a02a070f4feed7535ff
Aditya Agarwal <aditya.kgp25@gmail.com>
2020-02-20 00:05:36 -0500

    icp max iter to 60

--- sbpl_perception/src/scripts/tools/fat_dataset/fat_pose_image.py
+++ sbpl_perception/src/scripts/tools/fat_dataset/fat_pose_image.py
@@ -2543,7 +2543,7 @@ def run_ycb_6d(dataset_cfg=None):
     # for img_i in [0]:    
 
     IMG_LIST = np.loadtxt(os.path.join(image_directory, 'image_sets/keyframe.txt'), dtype=str).tolist()
-    for scene_i in range(49, 60):
+    for scene_i in range(48, 60):
         for img_i in range(1,2500):
         # for img_i in IMG_LIST:
         # for img_i in tuna_list:
